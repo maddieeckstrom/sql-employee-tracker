525cbac30c4527939db2bb00fb24482282873ebd
Maddie <maddieeckstrom@gmail.com>
2023-10-24 12:42:47 -1000

    added foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -11,6 +11,8 @@ CREATE TABLE role(
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee(
@@ -19,4 +21,10 @@ CREATE TABLE employee(
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
 );
+
+-- set up foreign keys
